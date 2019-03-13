a8da38a2616273a843ff1d1f6aa4321abe1d29f7
Justin Bricker <jt.bricker@gmail.com>
2019-03-13 11:26:47 -0400

    Migrate group_classes into package with tests

---
+++ tests/test_nbutils.py
@@ -0,0 +1,31 @@
+"""Tests for `nbutils` package."""
+import pytest
+
+import pandas as pd
+
+from mlexp import nbutils
+
+def test_reassign_classes_keep_all_data_but_reassign():
+    """Return all data but reassign the labels"""
+    data = pd.DataFrame([{'class':0,'data':100},{'class':1,'data':200},{'class':0,'data':500}])
+    new_data = nbutils.reassign_classes(data, {0:1,1:0}, 'class')
+
+    assert len(data.index) == len(new_data.index)
+    assert new_data['class'].value_counts()[0] == data['class'].value_counts()[1]
+    assert new_data['class'].value_counts()[1] == data['class'].value_counts()[0]
+
+def test_reassign_classes_keep_only_one_class():
+    """Return all data but reassign the labels"""
+    data = pd.DataFrame([{'class':0,'data':100},{'class':1,'data':200},{'class':0,'data':500}])
+    new_data = nbutils.reassign_classes(data, {0:0}, 'class')
+
+    assert len(new_data.index) == len(data[data['class'] == 0])
+    assert new_data['class'].value_counts()[0] == data['class'].value_counts()[0]
+
+def test_reassign_classes_three_groups_into_two():
+    """Return all data but reassign the labels"""
+    data = pd.DataFrame([{'class':0,'data':100},{'class':1,'data':200},{'class':0,'data':500},{'class':2,'data':400}])
+    new_data = nbutils.reassign_classes(data, {0:0, 1:1, 2:1}, 'class')
+
+    assert new_data['class'].value_counts()[0] == data['class'].value_counts()[0]
+    assert new_data['class'].value_counts()[1] == data['class'].value_counts()[1] + data['class'].value_counts()[2]
